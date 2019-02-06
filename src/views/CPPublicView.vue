6175650476e3b2058ce946fd3d4d92a96c37173c
LIAN <snografx@gmail.com>
2019-02-06 09:31:00 +0100

    Create Tools Images

--- src/views/CPPublicView.vue
+++ src/views/CPPublicView.vue
@@ -9,22 +9,28 @@
     <div class="anchor-links">
       <h4>Skip to</h4>
       <div class="skip-links case-links">
-        <a href="#" v-scroll-to="'#views'">
+        <a href="#" v-scroll-to="'#overview'">
           <div class="case-link-overview">Overview</div>
         </a>
         <a href="#" v-scroll-to="'#analysis'">
           <div class="case-link">Analysis</div>
         </a>
-        <a href="#" v-scroll-to="'#design'">
+        <a href="#" v-scroll-to="'#goals'">
+          <div class="case-link">Goals</div>
+        </a>
+        <a href="#" v-scroll-to="'#process'">
           <div class="case-link">Process</div>
         </a>
-        <a href="#" v-scroll-to="'#result'">
+        <a href="#" v-scroll-to="'#prototype'">
           <div class="case-link">Prototype</div>
+        </a>
+        <a href="#" v-scroll-to="'#dev'">
+          <div class="case-link">Development</div>
         </a>
       </div>
     </div>
 
-    <div id="views" class="case case-right case-overview">
+    <div id="overview" class="case case-right case-overview">
       <div class="visual">
         <div class="carousel-container">
           <carousel :perPage="1" :navigationEnabled="false" paginationColor paginationActiveColor>
@@ -57,25 +63,28 @@
         <h1>Mobile Create Tools</h1>
         <h4>Case Analysis</h4>
         <p>"Dialogue" is a CityPlanner module for creating geospatial forms. The mobile version launched in Q4 2019 without it's premier feature – Create Tools. These tools allow end users to place markers and draw lines and areas in the 3D world.</p>
-        <p>The missing feature was highly requested by our customers but – being much more complicated and interactive – it was planned for the second release so data and feedback could best inform development.</p>
+        <p>The missing feature was highly requested by our customers but – being much more complicated and interactive – it was planned for the second release so initial data and feedback could best inform further development.</p>
         <h6>Role: Product Design, UX &amp; Interface Design</h6>
       </div>
     </div>
 
     <ArrowUp/>
 
-    <div id="design" class="case case-left">
+    <div id="goals" class="case case-left">
       <div class="visual">
         <div class="carousel-container">
           <carousel :perPage="1" :navigationEnabled="false" paginationColor paginationActiveColor>
             <slide>
-              <div class="slide-ph">Web Version</div>
+              <img src="@/assets/CPPublicView/Create-Desktop-Label.png">
             </slide>
             <slide>
-              <div class="slide-ph">UX Persona</div>
+              <div class="slide-ph">Milanote Project Plan</div>
             </slide>
             <slide>
-              <div class="slide-ph">Wireframe</div>
+              <div class="slide-ph">Impact Map</div>
+            </slide>
+            <slide>
+              <div class="slide-ph">Web to Mobile Graphic?</div>
             </slide>
             <slide>
               <img src="@/assets/CaseSnografx-DropF1.png">
@@ -84,17 +93,47 @@
         </div>
       </div>
       <div class="about">
-        <h1>Design Process</h1>
-        <h4>User Research, Feedback, Prototype</h4>
-        <p>Initial designs were based on internal research and data from the ongoing development of the base platform. UX personas are based on close relationships with both customers and users. Our organisational priority was establishing mobile-first methodology.</p>
-        <p>Interface usability research focused primarily on blending drawing and map application conventions, while providing a fun end-user experience in order to provide the best value – measured by number of submissions – to our customers.</p>
+        <h1>Project Goals</h1>
+        <h4>Internal Research, User Feedback, Challenges</h4>
+        <p>The goal of the project is to satisfy our customers directly by addressing their feature requests and indirectly by increasing traffic – especially the number of end-user submissions. This adds value to the dialogue module and drives sales.</p>
+        <p>We aimed to achieve this by delivering a fun product to the most popular and convenient platform. The biggest design challenges were translating complex interface requirements to the small screen in an uncluttered and intuitive fashion.</p>
+        <h6>Tools: Milanote</h6>
+      </div>
+    </div>
+
+    <ArrowUp/>
+
+    <div id="process" class="case case-right">
+      <div class="visual">
+        <div class="carousel-container">
+          <carousel :perPage="1" :navigationEnabled="false" paginationColor paginationActiveColor>
+            <slide>
+              <div class="slide-ph">Early Prototype?</div>
+            </slide>
+            <slide>
+              <div class="slide-ph">UX Persona x2</div>
+            </slide>
+            <slide>
+              <div class="slide-ph">Design Inspiration: Google Maps, Adobe Mobile?</div>
+            </slide>
+            <slide>
+              <img src="@/assets/CaseSnografx-DropF1.png">
+            </slide>
+          </carousel>
+        </div>
+      </div>
+      <div class="about">
+        <h1>Research &amp; Process</h1>
+        <h4>Priorities, Personas &amp; Design Investigation</h4>
+        <p>Initial designs were based on internal research and data from the ongoing development of the base platform. UX personas are based on close relationships with both customers and end-users.</p>
+        <p>Interface usability research focused primarily on blending drawing and map application conventions. An organisational priority of adopting mobile-first solutions which can scale up to the base platform were an important consideration.</p>
         <h6>Tools: Figma, XD</h6>
       </div>
     </div>
 
     <ArrowUp/>
 
-    <div id="result" class="case case-right">
+    <div id="prototype" class="case case-left">
       <div class="visual">
         <div class="carousel-container">
           <carousel :perPage="1" :navigationEnabled="false" paginationColor paginationActiveColor>
@@ -105,20 +144,26 @@
               <img src="@/assets/CPPublicView/Create-Prototype.png">
             </slide>
             <slide>
-              <div class="slide-ph">Pre &amp; Post-Modal</div>
+              <img src="@/assets/CPPublicView/Create-Milenote-V4.png">
             </slide>
             <slide>
-              <div class="slide-ph">Pre &amp; Post-Exploding Button</div>
+              <img src="@/assets/CPPublicView/Create-Notes-V4.png">
+            </slide>
+            <slide>
+              <div class="slide-ph">Closeup</div>
+            </slide>
+            <slide>
+              <div class="slide-ph">Before &amp; After Improvement Examples</div>
             </slide>
           </carousel>
         </div>
       </div>
 
       <div class="about">
-        <h1>Prototype &amp; Development</h1>
-        <h4>Currently Under Development</h4>
-        <p>Internal wireframes were first created and tested in XD. Feedback informed advanced prototypes, developed in Figma, for sharing with stakeholders plus user and device testing.</p>
-        <p>Early insights gained include adjusting the position of the buttons away from the edge of the screen and adopting modal dialogs. The final design has been approved and development is underway. Development versions are currently being being tested and polished before release.</p>
+        <h1>Prototype</h1>
+        <h4>Testing &amp; Refining Interface</h4>
+        <p>Basic wireframes of different layout styles were tested in Adobe XD. Internal feedback informed advanced prototypes, developed in Figma, for presentation to stakeholders as well as user and device testing.</p>
+        <p>Workshopping prototypes with test-users yielded many improvements such as changing the location and style of modal windows, streamlining select and delete workflows, and addressing minor design inconsistancies.</p>
         <div class="case-links">
           <a
             href="https://www.figma.com/file/r7GWbuS9T2ZnCb1kVvofCj/Mobile-Create-Tools?node-id=0%3A1"
@@ -133,7 +178,19 @@
             <div class="external-link">Try Prototype</div>
           </a>
         </div>
-        <h6>Release: Scheduled Q1 2019</h6>
+        <h6>Tools: Figma, XD</h6>
+      </div>
+    </div>
+
+    <ArrowUp/>
+
+    <div id="dev" class="case case-text">
+      <div class="about">
+        <h1>Development</h1>
+        <h4>Scheduled Release: Q1 2019</h4>
+        <p>As of writing this article, the project has moved into the development phase. A final prototype was created in Figma and shared with our development team, along with a Zeplin export.</p>
+        <p>Development is planned and managed over bi-weekly sprints, with continual testing and design improvements.</p>
+        <h6>Tools: Figma, Zeplin, Jira/Kanban</h6>
       </div>
     </div>
 
