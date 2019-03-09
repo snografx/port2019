a8b4ed9835f8a65ea9b89f9b4d273530ed64d9e7
LIAN <snografx@gmail.com>
2019-03-09 15:27:37 +0100

    Snögrafx F

--- src/components/CaseSnografx.vue
+++ src/components/CaseSnografx.vue
@@ -10,7 +10,7 @@
         <router-link to="/snografx">Snögrafx</router-link>
       </h1>
       <h4>2002 &mdash; Present (Part-Time Since 2012)</h4>
-      <p>Snögrafx was founded in Montréal in 2002 as an innovative web &amp; graphic design agency. It was my full time job until relocating to Stockholm a decade later in 2012.</p>
+      <p>Snögrafx was founded in Montréal in 2002 as an innovative web &amp; graphic design agency. It was my full-time job until relocating to Stockholm a decade later in 2012.</p>
 
       <div class="case-links">
         <router-link to="/snografx">
--- src/views/Snografx.vue
+++ src/views/Snografx.vue
@@ -2,7 +2,7 @@
   <div class="text-shrink">
     <Title
       title="Snögrafx"
-      blurb="Snögrafx was founded in Montréal in 2002 as an innovative web &amp; graphic design agency. It was my full time job until relocating to Stockholm a decade later in 2012. Since switching to part-time, Snögrafx works infrequently – primarily on small and varied visual design projects, usually for long time clients."
+      blurb="Snögrafx was founded in Montréal in 2002 as an innovative web &amp; graphic design agency. It was my full-time job until relocating to Stockholm a decade later in 2012. Since switching to part-time, Snögrafx works less frequently – primarily on small and varied visual design projects, usually for long time clients."
       tags
     />
 
@@ -116,11 +116,9 @@
       </div>
       <div class="about">
         <h1>Web Design &amp; Development</h1>
-        <p>The primary focus in early years was designing and developing pixel perfect project-specific websites, complete with a custom built back-ends and a sharp focus on web standards and usability.</p>
-        <p>I am proud to have pioneered and championed usability and flat design more than a decade before it's rise to prominence. In later years, I created my own content management system and eventually shifted to developing Wordpress themes and plugins.</p>
-        <h6>
-          <br>
-        </h6>
+        <p>The primary focus in early years was designing and developing pixel-perfect bespoke websites, complete with custom-built site management and a sharp focus on web standards and usability.</p>
+        <p>I am proud to have pioneered and championed usability and flat design more than a decade before it's rise to prominence. I later developed my own content management system and built made-to-order WordPress themes and plugins. I've recently learned Vue.js for single page applications.</p>
+        <h6>Vue.js, WordPress, Custom Development</h6>
       </div>
     </div>
 
@@ -215,11 +213,9 @@
       </div>
       <div class="about">
         <h1>Visual &amp; Graphic Design</h1>
-        <p>As a small independent studio, great visual design has always been the ace up my sleeve. While many are technically capable, I learned technical skills like website development to facilitate good design.</p>
-        <p>I developed a successful niche by providing a then unique service of packaging top-notch interactive digital products with high quality design work, specifically identity, graphic, and print design.</p>
-        <h6>
-          <br>
-        </h6>
+        <p>As a small independent studio, great visual design has always been the ace up my sleeve. While many are technically capable, I learned technical skills like website development to facilitate great design.</p>
+        <p>I developed a successful niche by providing a then unique service of packaging top-notch interactive digital products with high-quality design work – specifically logo, graphic, and print design.</p>
+        <h6>Logo &amp; Identity, Motion &amp; Video, Icon, Print, Digital</h6>
       </div>
     </div>
 
@@ -316,7 +312,7 @@
         <h1>Labrador Records</h1>
         <h4>Customer Spotlight</h4>
         <p>I have been working with Labrador Records in one capacity on another since before the turn of the century.</p>
-        <p>In that time I have worked on websites and print design for the label and it's esteemed stable of artists, including the indie legends like The Radio Dept., Club 8, The Mary Onettes, Acid House Kings, The Legends, and more.</p>
+        <p>In that time I have worked on websites, digital projects, and print design for the label and it's esteemed stable of artists, including indie legends like The Radio Dept., Club 8, The Mary Onettes, Acid House Kings, The Legends, and more.</p>
         <div class="case-links">
           <a href="http://labrador.se/" target="_blank">
             <div class="external-link">Website</div>
@@ -438,7 +434,7 @@
         <h1>Teach a Man to Fish</h1>
         <h4>Customer Spotlight</h4>
         <p>I was first commissioned by TAMTF to develop their identity in 2007.</p>
-        <p>In time that relationship led to a trilingual website, further brand development, print / event design, and more. It's been a privilege to play a role on the development of this remarkable organisation.</p>
+        <p>In time, that relationship led to a trilingual website, continuing brand development, print/event design, and more. It's been a privilege to play a role in the development of this remarkable organisation.</p>
         <h6>Brand/Identity, Graphic Design, Website, Print Design</h6>
       </div>
     </div>
@@ -452,7 +448,7 @@
             <slide>
               <img src="@/assets/Snografx/Snografx-Com.png">
               <div class="lb-caption">
-                <h6>Snografx.com (2013)</h6>
+                <h6>Snografx.com (2014)</h6>
               </div>
               <a
                 class="lb-button"
@@ -473,7 +469,7 @@
         <h4>Web &amp; Graphic Design Since 2002</h4>
         <p>
           Want to see more? Visit
-          <a href="http://snografx.com" target="_blank">snografx.com</a> – the website has not been updated since 2014, but serves as both a time capsule of archived projects and an illustration of how well quality design stands the test of time.
+          <a href="http://snografx.com" target="_blank">snografx.com</a> – the website has not been updated since 2014 but serves as both a time capsule of archived projects and an illustration of how well quality design stands the test of time.
         </p>
         <div class="case-links">
           <a href="http://snografx.com" target="_blank">
